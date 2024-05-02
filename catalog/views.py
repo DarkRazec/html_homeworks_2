3f9f1e8a6a0438a00f956d6ef83a8673ae2363de
DarkRazec <darkrazec@yahoo.com>
2024-05-02 22:08:57 +0300

    trying to do something with new features and site pages

--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 
+from catalog.models import Product, Contact
+
 
 def homepage(request):
-    return render(request, 'catalog/homepage.html')
+    products = (Product.objects.all()).reverse()
+    return render(request, 'catalog/homepage.html', locals())
 
 
 def contact_info(request):
+    contacts = Contact.objects.all()
     if request.method == 'POST':
-        name = request.POST.get('name')
+        name = request.POST.get('country')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         with open('data/contact.txt', 'a', encoding='utf-8') as f:
             f.write(f"{name} {phone} {message}\n")
 
-    return render(request, 'catalog/contact_info.html')
+    return render(request, 'catalog/contact_info.html', locals())
