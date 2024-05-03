1839f542e667943ed8a81108cbdeb66501a5eef2
DarkRazec <darkrazec@yahoo.com>
2024-05-03 17:44:51 +0300

    fixed 'country' bug

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -2,7 +2,7 @@ import json
 
 from django.core.management import BaseCommand
 
-from catalog.models import Category, Contact
+from catalog.models import Category, Product
 
 
 class Command(BaseCommand):
@@ -10,18 +10,18 @@ class Command(BaseCommand):
     @staticmethod
     def json_read_categories():
         # Получаем данные из фикстур с категориями
-        with open('data/category_data.json', encoding='utf-16') as f:
+        with open('data/category_data.json', encoding='utf-8') as f:
             return json.load(f)
 
     @staticmethod
     def json_read_products():
         # Получаем данные из фикстур с продуктами
-        with open('data/product_data.json', encoding='utf-16') as f:
+        with open('data/product_data.json', encoding='utf-8') as f:
             return json.load(f)
 
     def handle(self, *args, **options):
 
-        Contact.objects.all().delete()
+        Product.objects.all().delete()
         Category.objects.all().delete()
 
         # Списки для хранения объектов
@@ -33,13 +33,13 @@ class Command(BaseCommand):
         # Создаем объекты в базе с помощью метода bulk_create()
         Category.objects.bulk_create(category_for_create)
 
-        product_for_create = [Contact(
+        product_for_create = [Product(
             id=product['pk'],
-            name=product['fields']['country'],
+            name=product['fields']['name'],
             desc=product['fields']['desc'],
             category=Category.objects.get(pk=product['fields']['category']),
             price=product['fields']['price']
         ) for product in Command.json_read_products()]
 
         # Создаем объекты в базе с помощью метода bulk_create()
-        Contact.objects.bulk_create(product_for_create)
+        Product.objects.bulk_create(product_for_create)
